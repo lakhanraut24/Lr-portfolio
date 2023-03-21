c01b157d6e44583d99e25516166f2a46d25106a0
LakhanRaut <lakhanraut24@gmail.com>
2023-03-21 16:41:57 +0530

    portfolio project completed

---
+++ src/data/data.js
@@ -0,0 +1,49 @@
+import calc from '../assets/projects/calc.png'
+import ecom from '../assets/projects/simple-ecom.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:calc,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:ecom,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:calc,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Simple E-com Application",
+        image:ecom,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:calc,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:calc,
+        github:"",
+        live:"",
+    },
+
+
+]
