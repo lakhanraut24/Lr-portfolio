cde17c016483df3b093c896a3e3cf1fdd6d8692e
LakhanRaut <lakhanraut24@gmail.com>
2025-02-24 15:03:53 +0530

    all file uploded

--- src/data/data.js
+++ src/data/data.js
@@ -1,47 +1,53 @@
-import calc from "../assets/projects/calc.png";
-import ecom from "../assets/projects/simple-ecom.png";
+import simple_cart from "../assets/projects/simple_cart.png";
+import LR_ecommerce from "../assets/projects/LR_ecommerce.png";
+import Order_Manage from "../assets/projects/Order_Manage.png";
+import simple_ecom from "../assets/projects/simple_ecom.png";
+import categories from "../assets/projects/categories.png";
+import fullstack_next from "../assets/projects/fullstack_next.png";
+
+
 
 export const data = [
   {
     id: 1,
-    name: "React JS Application",
-    image: calc,
-    github: "",
-    live: "",
+    name: "Next JS Application",
+    image: LR_ecommerce,
+    github: "https://github.com/lakhanraut24/next-ecom",
+    live: "https://next-ecom-qe1n.vercel.app/",
   },
   {
     id: 2,
     name: "React JS Application",
-    image: ecom,
-    github: "",
-    live: "",
+    image: simple_cart,
+    github: "https://github.com/lakhanraut24/Simple-E-commerce-",
+    live: "https://simple-e-commerce-lovat.vercel.app/",
   },
   {
     id: 3,
     name: "React JS Application",
-    image: calc,
-    github: "",
-    live: "",
+    image: Order_Manage,
+    github: "https://github.com/lakhanraut24/order-management-web-app",
+    live: "https://order-management-web-app-a9j2.vercel.app/",
   },
   {
     id: 4,
     name: "React JS Simple E-com Application",
-    image: ecom,
-    github: "",
-    live: "",
+    image: simple_ecom,
+    github: "https://github.com/lakhanraut24/React-E-commerce-project",
+    live: "https://react-e-commerce-project-oazu.vercel.app/",
   },
   {
-    id: 3,
+    id: 5,
     name: "React JS Application",
-    image: calc,
-    github: "",
-    live: "",
+    image: categories,
+    github: "https://github.com/lakhanraut24/React-E-commerce-project/tree/master/Furns-Ecom",
+    live: "https://react-e-commerce-project.vercel.app/",
   },
   {
-    id: 3,
-    name: "React JS Application",
-    image: calc,
-    github: "",
-    live: "",
+    id: 6,
+    name: "Next-js Application",
+    image: fullstack_next,
+    github: "https://github.com/lakhanraut24",
+    live: "https://lr-portfolio.vercel.app/",
   },
 ];
